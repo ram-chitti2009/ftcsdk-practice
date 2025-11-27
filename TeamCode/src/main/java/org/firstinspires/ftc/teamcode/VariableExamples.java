a0921435ef4dbe09e2d11ce9ad9352496a71aa15
ram-chitti2009 <venkata.chitti19800101@gmail.com>
2025-11-27 04:53:50 -0600

    Add practice Java classes for FTC team code
    Added GamePadPractice, VariableExamples, and VariablePractice classes to demonstrate gamepad input handling and variable usage for FTC robotics programming. These files provide examples for telemetry output and basic variable declarations.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VariableExamples.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode;
+
+public class VariableExamples {
+
+    int robotHeight;
+    double motorSpeed;
+
+    boolean clawClosed;
+
+    robotHeight = 10;
+    motorSpeed = 0.5;
+    clawClosed= true
+
+
+}
